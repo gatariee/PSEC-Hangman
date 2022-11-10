80c9d9f15cca389e91d390db1fc3793bff0a5eee
gatari <79693291+deceptivecz@users.noreply.github.com>
2022-11-10 16:35:48 +0800

    fixed and added some stuff
    + added word_list.txt
    + added game_logs.txt
    + added game_settings.txt
    - removed wordlist.txt
    + fixed bug with name validation
    + name validation now iterates through existing names in game_logs.txt
    + admin.py wordlist editing works properly with empty wordlists now
    + reports are now generated after finished game
    + formatting fixes (added -'s)
    + cleaned up some ugly functions

--- admin.py
+++ admin.py
@@ -1,19 +1,26 @@
 import ast
 import json
-newWord = {
-    'word': input('word: '),
-    'type': input('type: '),
-    'difficulty': input('difficulty: ')
-}
-with open('wordlist.txt', 'r+') as f:
-    try: 
-        obj = (ast.literal_eval(f.read()))
-        obj.append(newWord)
+import os
+def addWord():
+    newWord = {
+        'word': input('word: '),
+        'type': input('type: '),
+        'difficulty': input('difficulty: ')
+    }
+    try:
+        with open('word_list.txt', 'r+') as f:
+            if(os.stat("word_list.txt").st_size == 0):
+                obj = []
+                obj.append(newWord)
+            else:                                       
+                obj = (ast.literal_eval(f.read()))
+                obj.append(newWord)
+            new = json.dumps(obj, indent=4)
+            f.seek(0)
+            f.truncate(0)
+            f.write(str(new))
+            f.close()
+            return
     except:
-        obj = newWord
-    new = json.dumps(obj, sort_keys=True, indent=4)
-    f.seek(0)
-    f.truncate(0)
-    f.write(str(new))
-    f.close()
-print(type(obj))
+        print("Error. Wordlist not found. ")
+        return
--- hangman.py
+++ hangman.py
@@ -1,5 +1,8 @@
 import ast
 import random
+from datetime import date
+import json
+import os
 class Game:
     def __init__(self, player):
         self.player = player
@@ -9,33 +12,63 @@ class Game:
         self.previous_guesses = []
         self.incorrect_list = []
         self.guess_progress = "_ " * len(self.word)
+        self.totalPoints = 0
+    def calculatePoints(self):
+        return len((self.guess_progress).replace("_", " ").replace(" ", "")) * 2
+    def endGame(log):
+        def generateReport():
+            with open('game_logs.txt', 'r+') as f:
+                if(os.stat("game_logs.txt").st_size == 0):
+                    obj = []
+                    obj.append(log)
+                else:
+                    obj = (ast.literal_eval(f.read()))
+                    obj.append(log)
+                new = json.dumps(obj, indent=4)
+                f.seek(0)
+                f.truncate(0)
+                f.write(str(new))
+                f.close()
+                return
+        def displayReport():
+            print(f"Here is what we are logging: \n------------------------------------------\nName: {log['player']}\nPoints: {log['points']}\nDate: {log['date']}\n------------------------------------------")
+        print(f"------------------------------------------\nGame Over. You have ended the game with {log['points']} points.") 
+        generateReport()
+        displayReport()
     def guessLetter(self, letter):
         def verifyGuess(guess):
             if(not guess.isalpha()):
-                print("Please enter a letter.")
+                print("------------------------------------------\nPlease enter a letter.\n------------------------------------------")
             elif(len(guess) != 1):
-                print("Please enter a single letter.")
+                print("------------------------------------------\nPlease enter a single letter. \n------------------------------------------")
             elif(guess in self.previous_guesses):
-                print("You already guessed that letter.")
+                print("------------------------------------------\nYou already guessed that letter. \n------------------------------------------")
             else:
                 return True
         if(not verifyGuess(letter)):
             return
         else:
             letter = letter.lower()
+        count = 0
         if letter in self.word:
             for i in range(len(self.word)):
                 if self.word[i] == letter:
                     self.guess_progress = self.guess_progress[:i*2] + letter + self.guess_progress[i*2+1:]
+                    count+=1
         else:
             self.guesses += 1
             self.incorrect_list.append(letter)
+            print("------------------------------------------\nIncorrect! You have " + str(5-self.guesses) + " guesses left. \n------------------------------------------")
         self.previous_guesses.append(letter)
+        if(count > 1):
+            print("------------------------------------------\nCongratulations. There are " + str(count) + " " + letter + "'s. \n------------------------------------------")
+        elif(count == 1):
+            print("------------------------------------------\nCongratulations. There is 1 " + letter + ". \n------------------------------------------")
     def pickWord():
         def readWords():
             global previous_words
             try:
-                with open('wordlist.txt') as f:
+                with open('word_list.txt') as f:
                     wordlist = (ast.literal_eval(f.read()))
                     return wordlist  
             except:
@@ -61,27 +94,47 @@ def main(): # Start of program
         special_chars = ['-','/']
         allowed_chars = alphabets_upper + alphabets_lower + special_chars
         playerName = input("Enter your name: ")
-        if(all(char in allowed_chars for char in playerName)):
-            begin(playerName)
-            return
-        print("Please enter a valid name. ")
+        if(not all(char in allowed_chars for char in playerName)):
+            print("Please enter a valid name. ")
+            continue
+        else: 
+            count = 0
+            if(os.stat("game_logs.txt").st_size != 0):
+                with open("game_logs.txt", 'r') as f:
+                    gameLogs = (ast.literal_eval(f.read()))
+                    for value in gameLogs:
+                        if(value['player'] == playerName):
+                            break
+                        else:
+                            count+=1
+                if(count == len(gameLogs)):
+                    begin(playerName)
+                    return
+            else:
+                begin(playerName)
+                return
+            print("Name is taken. Please try again.")
 def begin(playerName): # Start of game loop
     global session
     HangMan = Game(playerName)
+    log['player'] = HangMan.player
     if(session == 1):
         print("\nWelcome to Hangman, " + HangMan.player + "!")
         print("Your word is " + str(len(HangMan.word)) + " letters long. ")
         print("You have 5 guesses to guess the word. Good Luck!")
-        print("Session: " + str(session))
+        print("Session: " + str(session) + "\n------------------------------------------")
     else:
         print("\nYour word is " + str(len(HangMan.word)) + " letters long. ")
-        print("Session: " + str(HangMan.session))
+        print("Session: " + str(HangMan.session) + "\n------------------------------------------")
     while(HangMan.guesses < 5) and ((HangMan.guess_progress).replace(" ","") != HangMan.word):
+        HangMan.calculatePoints()
         print("\nWord: " + HangMan.guess_progress)
         print(f"Incorrect Guesses: {', '.join(HangMan.incorrect_list)}")
+        print(f"Correct Guesses: {', '.join(list(set(HangMan.previous_guesses) - set(HangMan.incorrect_list)))}")
         print("Guesses remaining: " + str(5 - HangMan.guesses))
         guess = input("Guess: ")
         HangMan.guessLetter(guess)
+    
     if(HangMan.guesses == 5):
         print("\nYou lose.")
     else:
@@ -89,8 +142,16 @@ def begin(playerName): # Start of game loop
     print(f"The {HangMan.type} is \"{HangMan.word}\", the difficulty was {HangMan.difficulty}.")
     if(session < 3):
         session+=1
+        log['points'] += HangMan.calculatePoints()
+        print(f"Your total points are {log['points']}.")
         begin(playerName)
+    else:
+        log['points'] += HangMan.calculatePoints()
+        Game.endGame(log=log)
+
+
     
 
 if(__name__ == "__main__"):
+    log = {'player': '','points': 0, 'date': date.today().strftime("%d/%m/%y")}
     main()
